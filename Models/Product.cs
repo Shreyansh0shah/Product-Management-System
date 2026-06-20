2c30d3204ba259cae13de936fd205cf58e3fa011
Shah shreyansh <151755121+Shreyansh0shah@users.noreply.github.com>
2026-06-20 17:19:53 +0530

    Add files via upload

---
+++ Models/Product.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ProductApp1.Models
+{
+    public class Product
+    {
+        public int ProductId { get; set; }
+
+        [Required]
+        public string Name { get; set; }
+
+        public string Category { get; set; }
+        public decimal Price { get; set; }
+        public int Quantity { get; set; }
+    }
+}
---
+++ Models/ProductContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace ProductApp1.Models
+
+
+{
+    public class ProductContext : DbContext
+    {
+        public ProductContext(DbContextOptions<ProductContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Product> Products { get; set; }
+    }
+}
